9024af5ed07ed18762481bf49652ce1fe0a0c6f9
Николай <73244772+ManenkoMykola@users.noreply.github.com>
2020-11-16 13:36:37 +0200

    Add files via upload

---
+++ лабараторна 6.py
@@ -0,0 +1,21 @@
+клас  Box :
+    def  __init__ ( self , value ):
+        себе . значення  =  значення
+
+    def  __bool__ ( self ):
+        якщо  я . значення  ==  1 :
+            повернути  True
+        ще :
+            повернення  False
+
+box  =  Box ( 0 )
+result  =  bool ( поле )
+друк ( результат )
+
+box  =  Box ( 1 )
+result  =  bool ( поле )
+друк ( результат )
+
+box  =  Box ( 2 )
+result  =  bool ( поле )
+друк ( результат )
