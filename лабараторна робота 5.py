31abba4cb59335097a3811975610974c715e4eec
Николай <73244772+ManenkoMykola@users.noreply.github.com>
2020-12-08 07:04:13 +0200

    Add files via upload

---
+++ лабараторна робота 5.py
@@ -0,0 +1,11 @@
+str=' весело сумно'
+x=str.split()
+print(x)
+str='жовтий чорний оранжевий рожевий червоний зелений'
+y=str.split()
+print(y)
+str='грузопідйомна машина'
+z=str.split()
+print(z)
+import random
+print(random.choice(x)+' '+ random.choice(y)+' '+ random.choice(z))
